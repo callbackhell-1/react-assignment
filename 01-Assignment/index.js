2d86d25f17c69d832d231999e74cd408dad1acbd
Adarsh <jha01adarsh@gmail.com>
2024-01-16 20:30:55 +0530

    Created nested React Elements

--- 01-Assignment/index.js
+++ 01-Assignment/index.js
@@ -1,8 +1,29 @@
 /*
 Using React :
-1. Create an Element
+1. Create nested React Elements
+example :
+<div id="parent">
+    <div id="child1">
+        <h2>1st child from child1</h2>
+        <h2>2nd child from child1</h2>
+    </div>
+    <div id="child2">
+        <h2>1st child from child2</h2>
+        <h2>2nd child from child2</h2>
+    </div>
+</div>
 */
 
-const heading = React.createElement("h1", {}, "Hello World from react");
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h2", {}, "1st child from child1"),
+    React.createElement("h2", {}, "2nd child from child1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h2", {}, "1st child from child2"),
+    React.createElement("h2", {}, "1st child from child2"),
+  ]),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
