696797170bdd6b240b06e3458b42a6d2cd92773f
Adarsh <jha01adarsh@gmail.com>
2024-01-17 17:15:55 +0530

    Make a Header using functional components

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/**
- * 1. Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class “title”)
- */
-
-const header = React.createElement("div", { class: "title" }, [
-  React.createElement("h1", {}, "Heading 1"),
-  React.createElement("h2", {}, "Heading 2"),
-  React.createElement("h3", {}, "Heading 3"),
-]);
-
-/**
- * Create the same element using JSX
- */
-const header1 = (
-  <div className="title">
-    {console.log("Hello world")},<h1 className="h-1">Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-  </div>
-);
-
-/**
- * Create a functional component of the same with JSX
- */
-
-const Header2 = () => {
+// Left
+const Logo = () => {
   return (
-    <div className="title">
-      <h1 className="head-1">Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-    </div>
+    <img
+      style={{ width: "50px" }}
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfUlGcxUmqLQ08e_4msz7RwXoNtq6yR_f_PA&usqp=CAU"
+    />
   );
 };
 
-const Footer = () => {
+// Middle
+const SearchBar = () => {
   return (
-    <div
-      className="title"
-      style={{ backgroundColor: "pink", border: "5px solid black" }}
-    >
-      <h1 className="head-1">Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
+    <input
+      style={{ height: "50px", width: "150px" }}
+      placeholder="Search your Items"
+      type="text"
+    ></input>
+  );
+};
+// Right
+const UserIcon = () => (
+  <img
+    style={{ width: "50px" }}
+    src="https://cdn-icons-png.flaticon.com/128/1144/1144760.png"
+  />
+);
+
+// Main
+const Main = () => {
+  return (
+    <div className="main-div">
+      <Logo />,
+      <SearchBar />,
+      <UserIcon />
     </div>
   );
 };
-
-const Main = () => (
-  <div>
-    <Header2 />,
-    <Footer />
-  </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Main />);
